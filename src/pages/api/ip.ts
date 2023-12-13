a37c1afb140bdbabfa50090a88b81307a4dbeda0
Arslan Thaheem <thaheem001@gmail.com>
2023-12-13 18:56:41 +0500

    ip

--- src/pages/api/ip.ts
+++ src/pages/api/ip.ts
@@ -1,8 +1,17 @@
+import * as geoip from 'geoip-lite';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const handle = async (req: any, res: any) => {
     const clientIp =
         req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    res.status(200).json({ ip: clientIp });
+    const geo = geoip.lookup(clientIp);
+
+    geo
+        ? res.status(200).json({ ip: clientIp, country: geo })
+        : res.status(400).json({
+              ip: clientIp,
+              country: { error: 'Unable to determine location.' }
+          });
 };
 
 export default handle;
