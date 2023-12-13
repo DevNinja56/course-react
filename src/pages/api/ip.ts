23225da02955a0ca034689e1ce84a16112d8bd56
Arslan Thaheem <thaheem001@gmail.com>
2023-12-13 18:48:42 +0500

    ip

--- src/pages/api/ip.ts
+++ src/pages/api/ip.ts
@@ -1,4 +1,8 @@
-export default async (req: any, res: any) => {
-    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const handle = async (req: any, res: any) => {
+    const clientIp =
+        req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.status(200).json({ ip: clientIp });
 };
+
+export default handle;
