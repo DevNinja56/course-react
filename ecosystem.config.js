21237dddc2aad6f9023f640c81ff08e97a31acb3
Arslan Arjumand <arslanarjumand012@gmail.com>
2024-02-16 12:50:47 +0500

    update pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: 'frontend',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-      instances: '1',
-      autorestart: true,
-      exec_mode: 'fork',
-      watch: true,
-      max_memory_restart: '1G'
-    }
-  ]
+    apps: [
+        {
+            name: 'frontend',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start',
+            instances: 1,
+            autorestart: true,
+            exec_mode: 'fork',
+            watch: true,
+            max_memory_restart: '300M',
+            out_file: './out.log',
+            error_file: './error.log',
+            merge_logs: true,
+            ignore_watch: ['node_modules', 'upload', 'out.log', 'error.log']
+        }
+    ]
 };
