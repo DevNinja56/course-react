c2db01b72a18f7bb669774ae145fb74549658ef7
Asad234322 <asad_ismail@ragzon.com>
2024-02-27 18:52:39 +0500

    conflict fixed in submiited files

--- src/components/Profile/Application/SubmitDocument.tsx
+++ src/components/Profile/Application/SubmitDocument.tsx
@@ -1,14 +1,21 @@
 import RequirementBox from '@/components/course/RequirementBox';
 import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { ROUTES } from '@/config/constant';
 
 const SubmitDocument = () => {
+    const router = useRouter();
+    const { id } = router.query;
     return (
         <div className="w-full">
             <div className="w-full md:w-1/2 py-5">
                 <p className="text-2xl font-semibold uppercase text-darkGrayColor py-3">
                     Identity
                 </p>
-                <RequirementBox url="" text="Passport" />
+                <Link href={`${ROUTES.FILE_SUBMIT}?id=${id}`}>
+                    <RequirementBox url="" text="Passport" />
+                </Link>
             </div>
             <div className="w-full">
                 <p className="text-[26px] font-bold text-mainTextColor pt-12 pb-3">
@@ -19,7 +26,10 @@ const SubmitDocument = () => {
                 </p>
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-7">
                     <RequirementBox text="Semester Marksheets" url="" />
-                    <RequirementBox text="Consolidated Marksheets" url="" />
+                    <Link href={`${ROUTES.CONSOLID_MARKSHEET}?id=${id}`}>
+                        {' '}
+                        <RequirementBox text="Consolidated Marksheets" url="" />
+                    </Link>
                     <RequirementBox text="Provisional Certificate" url="" />
                 </div>
             </div>
