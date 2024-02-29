a4f2360e16e69efb38e3baee86a59dd5b0f02b5c
Asad234322 <asad_ismail@ragzon.com>
2024-02-29 12:10:02 +0500

    modify functionality

--- src/store/slices/apply.slice.ts
+++ src/store/slices/apply.slice.ts
@@ -19,18 +19,6 @@ interface FileType {
     passportFile: FileDetails[];
     conSolidFile: FileDetails[];
 }
-// interface FileDetails {
-//     lastModified: number;
-//     lastModifiedDate: Date;
-//     name: string;
-//     size: number;
-//     type: string;
-// }
-
-// interface fileType {
-//     passportFile: FileDetails[];
-//     conSolidFile: any[];
-// }
 
 interface dataTypes {
     degree: objectType | null;
@@ -59,65 +47,56 @@ const apply = createSlice({
         addInstitute(state, action) {
             state.institute = action.payload;
         },
-        addFiles: (state, action: PayloadAction<{ type: 'passport' | 'conSolid'; filesArray: File[] }>) => {
-            state.files = produce(state.files, draft => {
+        addFiles: (
+            state,
+            action: PayloadAction<{
+                type: 'passport' | 'conSolid';
+                filesArray: File[];
+            }>
+        ) => {
+            state.files = produce(state.files, (draft) => {
                 if (action.payload.type === 'passport') {
-                    draft.passportFile.push(...action.payload.filesArray.map(file => ({
-                        lastModified: file.lastModified,
-                        lastModifiedDate: new Date(file.lastModified),
-                        name: file.name,
-                        size: file.size,
-                        type: file.type
-                    })));
+                    draft.passportFile.push(
+                        ...action.payload.filesArray.map((file) => ({
+                            lastModified: file.lastModified,
+                            lastModifiedDate: new Date(file.lastModified),
+                            name: file.name,
+                            size: file.size,
+                            type: file.type
+                        }))
+                    );
                 } else if (action.payload.type === 'conSolid') {
-                    draft.conSolidFile.push(...action.payload.filesArray.map(file => ({
-                        lastModified: file.lastModified,
-                        lastModifiedDate: new Date(file.lastModified),
-                        name: file.name,
-                        size: file.size,
-                        type: file.type
-                    })));
+                    draft.conSolidFile.push(
+                        ...action.payload.filesArray.map((file) => ({
+                            lastModified: file.lastModified,
+                            lastModifiedDate: new Date(file.lastModified),
+                            name: file.name,
+                            size: file.size,
+                            type: file.type
+                        }))
+                    );
                 } else {
                     console.log('Invalid file type:', action.payload.type);
                 }
             });
         },
-        // addFiles(
-        //     state,
-        //     action: PayloadAction<{ type: 'passport' | 'conSolid'; filesArray: File[] }>
-        // ) {
-        //     if (action.payload.type === 'passport') {
-        //         state.files = {
-        //             ...state.files,
-        //             passportFile: [
-        //                 ...state.files.passportFile,
-        //                 ...action.payload.filesArray
-        //             ]
-        //         };
-        //     }
-        //     else if(action.payload.type === 'conSolid'){
-        //         state.files = {
-        //             ...state.files,
-        //             conSolidFile: [
-        //               ...state.files.conSolidFile,
-        //                 ...action.payload.filesArray
-        //             ]
-        //         }
-        //     }  
-        //     else {
-        //         console.log('Invalid file type:');
-        //     } 
-        // },
-        removeFile(state, action: PayloadAction<{ type: 'passport' | 'conSolid'; index: number }>) {
+        removeFile(
+            state,
+            action: PayloadAction<{
+                type: 'passport' | 'conSolid';
+                index: number;
+            }>
+        ) {
             const { type, index } = action.payload;
             if (type === 'passport') {
-              state.files.passportFile.splice(index, 1);
+                state.files.passportFile.splice(index, 1);
             } else if (type === 'conSolid') {
-              state.files.conSolidFile.splice(index, 1);
+                state.files.conSolidFile.splice(index, 1);
             }
-          },
+        }
     }
 });
 
-export const { addDegree, addCourse, addInstitute, addFiles, removeFile } = apply.actions;
+export const { addDegree, addCourse, addInstitute, addFiles, removeFile } =
+    apply.actions;
 export default apply.reducer;
