797be862846c36747bd02a131fd9024b94538304
Asad234322 <asad_ismail@ragzon.com>
2024-03-01 15:36:12 +0500

    commit before master pull

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ['times-con.s3.us-east-2.amazonaws.com', "assets.timescoursefinder.com", 'picsum.photos', "lh3.googleusercontent.com","encrypted-tbn0.gstatic.com","images.unsplash.com","media.istockphoto.com","assets.jigsaw24.com"]
-  }
+    reactStrictMode: false,
+    images: {
+        domains: [
+            'times-con.s3.us-east-2.amazonaws.com',
+            'assets.timescoursefinder.com',
+            'picsum.photos',
+            'lh3.googleusercontent.com',
+            'encrypted-tbn0.gstatic.com',
+            'images.unsplash.com',
+            'media.istockphoto.com',
+            'assets.jigsaw24.com',
+            'd31cuupyjbe4e1.cloudfront.net'
+        ]
+    }
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
