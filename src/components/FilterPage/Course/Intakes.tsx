6fb61a5d9844d3b60c5fe06d9aba21213b655426
Asad Ismail <153621415+Asad234322@users.noreply.github.com>
2024-03-07 10:44:34 +0500

    Filter dynamic (#22)
    * chnaging
    
    * modify filters in cousres search on the base of filtes
    
    * add missing filters in responsive side bar
    
    * commit before master pull
    
    * changing
    
    * change query

--- src/components/FilterPage/Course/Intakes.tsx
+++ src/components/FilterPage/Course/Intakes.tsx
@@ -8,7 +8,14 @@ interface propsType {
     isLoading: boolean;
 }
 
-const IntakesFilter: React.FC<propsType> = ({ data, isLoading }) => {
+const IntakesFilter: React.FC<propsType> = ({ isLoading }) => {
+    const intakesFilterMonths = [
+        { intakes: ['Jan', 'Feb', 'Mar'] },
+        { intakes: ['Apr', 'May', 'Jun'] },
+        { intakes: ['Jul', 'Aug', 'Sep'] },
+        { intakes: ['Oct', 'Nov', 'Dec'] }
+    ];
+
     return (
         <div className="flex flex-col gap-y-3">
             <div className="flex justify-between items-center px-4 relative ">
@@ -20,13 +27,13 @@ const IntakesFilter: React.FC<propsType> = ({ data, isLoading }) => {
                 {isLoading ? (
                     <FilterCheckBoxLoader />
                 ) : (
-                    data
+                    intakesFilterMonths
                         .filter(({ intakes }) => intakes.length > 0)
                         .map(({ intakes: name }, i) => (
                             <FilterCheckBox
                                 key={'intakes--list--' + i + name.join(', ')}
                                 id={name.join(',')}
-                                text={name.join(' , ')}
+                                text={name.join(',')}
                                 name={'intakes'}
                                 value={name}
                             />
