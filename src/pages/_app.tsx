a828e11867128e4457f16859b95276df89a9b706
Arslan Thaheem <thaheem001@gmail.com>
2024-07-08 15:14:30 +0500

    add cache clear

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,21 @@ import '@/styles/globals.scss';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { deleteCookie } from '@/utils/cookies';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const componentLayout = (Component as any).layout as propsType;
+
+    useEffect(() => {
+        const lastCacheClear = localStorage.getItem('lastCacheClear');
+        if (lastCacheClear === null) {
+            localStorage.clear();
+            deleteCookie('token');
+            localStorage.setItem('lastCacheClear', new Date().toISOString());
+        }
+    }, []);
 
     return (
         <Provider store={store}>
